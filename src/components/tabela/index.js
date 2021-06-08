2bb4e1868b3fc00ca1ccd6638cf99782b35798ae
Gabriel Alves <gabriel.s.t.a.r.k@hotmail.com>
2021-06-08 19:57:12 -0300

    Mudança na tablea

--- src/components/tabela/index.js
+++ src/components/tabela/index.js
@@ -21,32 +21,54 @@ const Tabela = props => {
 
   return (
     <section className={Style.container}>
-      <table className={Style.tabela}>
-          <thead>
-          <tr>
-            <th>Nome</th>
-            <th>Peso</th>
-            <th>Altura</th>
-            <th>IMC</th>
-            <th>Categoria</th>
-          </tr>
-        </thead>
-        <tbody>
-          {listTest.map(item => {
-            return (
-              <tr key={item.nome}>
-                <td>{item.nome}</td>
-                <td>{`${item.peso} kg` }</td>
-                <td>{`${item.altura} m`}</td>
-                <td>{`${item.valueImc}`}</td>
-                <td>{item.categoria}</td>
-                <td className={Style.test}><MdDeleteForever cursor="pointer" size='20px'/></td>
+        <div className={Style.table}>
+          <div className={Style.option}>
+            <div>
+              <span>Tipo de método</span>
+              <select name="select">
+                <option value="bolha">Bolha</option>
+                <option value="sort" selected>Sort</option>
+                <option value="valor3">Valor 3</option>
+              </select>
+            </div>
+            <div>
+              <span>Ordenação</span>
+              <select name="select">
+                <option value="nome">Nome</option>
+                <option value="peso" selected>Peso</option>
+                <option value="altura">Altura</option>
+                <option value="imc">IMC</option>
+                <option value="categoria">Categoria</option>
+              </select>
+            </div>
+          </div>
+          <table className={Style.tabela}>
+            <thead>
+              <tr>
+                <th>Nome</th>
+                <th>Peso</th>
+                <th>Altura</th>
+                <th>IMC</th>
+                <th>Categoria</th>
               </tr>
-            )    
-            })
-          }
-        </tbody>
-      </table>
+            </thead>
+          <tbody>
+            {listTest.map(item => {
+              return (
+                <tr key={item.nome}>
+                  <td>{item.nome}</td>
+                  <td>{`${item.peso} kg` }</td>
+                  <td>{`${item.altura} m`}</td>
+                  <td>{`${item.valueImc}`}</td>
+                  <td>{item.categoria}</td>
+                  <td className={Style.test}><MdDeleteForever cursor="pointer" size='20px'/></td>
+                </tr>
+              )    
+              })
+            }
+          </tbody>
+        </table>
+        </div>
     </section>
   )
 }
