621c81f67256c4d8accd60083ed40e042398b2fc
Gabriel Alves <gabriel.s.t.a.r.k@hotmail.com>
2021-07-01 12:07:55 -0300

    Mudança nas válidações do formulário.

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -8,14 +8,19 @@ const Form = (props) => {
   const [valueId, setValueId] = useState(0);
 
 function test(event){
-  setlistForm({...listForm, [event.target.name]: event.target.value, id: valueId});
+  setlistForm({
+    ...listForm, 
+    [event.target.name]: event.target.name === 'nome' && event.target.value 
+    ? event.target.value[0].toUpperCase()  +  event.target.value.substr(1)
+    :  event.target.value > 0 && event.target.value,
+    id: valueId});
 }
 
 async function handleSubmit(event) {
-  
+  event.preventDefault();
   let formw = event.target.parentNode.querySelectorAll('form input')
   let newList = Array.prototype.slice.call(formw);
-  event.preventDefault();
+  
   if(newList.every(element => element.value !== "")){
       let newList = calcularImc(listForm)
       let listComplet =  verificarCategoria(newList);
@@ -34,7 +39,7 @@ async function handleSubmit(event) {
             <input type="text" placeholder="Nome" name="nome" value={listForm.nome} onChange={ test } />
             <input type="number" placeholder="Peso (kg)"  onChange={ test } name="peso" value={listForm.peso} />
             <input type="number" placeholder="Altura (m)" onChange={ test } name="altura" value={listForm.altura} />
-            <button onClick={handleSubmit} >Enviar</button>
+            <button onClick={handleSubmit}>Enviar</button>
           </form>
         </div>
       </section>
